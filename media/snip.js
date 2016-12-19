fce56d4284d079d98c8f739a6ca406669d10a7cf
Dzekunov Pavel <developer-pavlik@gimil.com>
2016-12-19 13:50:42 +0300

    Медиа запросы  в js

---
+++ media/snip.js
@@ -0,0 +1,11 @@
+if (window.matchMedia("(orientation: portrait)").matches) {
+    // you're in PORTRAIT mode
+}
+
+if (window.matchMedia("(orientation: landscape)").matches) {
+    // you're in LANDSCAPE mode
+}
+
+if (window.matchMedia("(max-width: 1216px) and (min-width: 763px)").matches) {
+    console.log("Ты ды");
+}
