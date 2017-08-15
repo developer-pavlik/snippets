73aa4c174026ae26e2b081865695af26c8122495
Dzekunov Pavel <developer-pavlik@gimil.com>
2017-08-15 15:45:34 +0300

    Г мап

---
+++ infobox_google_map/src/snip.js
@@ -0,0 +1,24 @@
+if (google) { // eslint-disable-line no-undef
+    const labelTemplate = '<div class="p-contacts__map-label">Federation tower East</div>';
+
+    let myOptions = {
+        content: labelTemplate,
+        disableAutoPan: true,
+        position: new google.maps.LatLng(55.7501635, 37.5382627), // eslint-disable-line no-undef
+        closeBoxURL: '',
+        isHidden: false,
+        pane: 'mapPane',
+        enableEventPropagation: true
+    };
+
+    let ibLabel = new InfoBox(myOptions); // eslint-disable-line no-undef
+    ibLabel.open(map);
+}
+
+google.maps.event.addDomListener(window, 'resize', function () { // eslint-disable-line no-undef
+    centerMapSelector();
+    google.maps.event.trigger(map, 'resize'); // eslint-disable-line no-undef
+    map.setCenter(new google.maps.LatLng(centerMapCoordinates[0], centerMapCoordinates[1])); // eslint-disable-line no-undef
+});
+
+
